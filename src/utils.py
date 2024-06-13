79cbc580d67eddd8634bcfcf1f9a82a7829fbb7d
apossya <toni2607@mail.ru>
2024-06-13 18:08:52 +0000

    new file:   src/__pycache__/predict_app.cpython-310.pyc 	new file:   src/__pycache__/utils.cpython-310.pyc 	modified:   src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,6 @@ import numpy as np
 
 def predict_io_bounded(area):
     """Emulate io delay"""
-    time.sleep(1)
     avg_price = 200_000                 # RUB / m2
     return int(area * avg_price)
 
